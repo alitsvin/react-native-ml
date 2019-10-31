42b6e12aca8fa5579f34b00961cb9dec1cf2e233
Litsvin <alitsvin@i-deasoft.by>
2019-10-31 16:08:12 +0300

    ft: installed RNCamera

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -7,11 +7,12 @@ import {
   StyleSheet,
   PermissionsAndroid
 } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
 import * as mobilenet from '@tensorflow-models/mobilenet';
 
-const Main = (props: any): React.JSXElement => {
+const Main = (props: any): React.FunctionComponentElement<any> => {
   const [loading, setLoading] = useState(true);
   const [cameraPermissionGranted, setCameraPermission] = useState(false);
   const [predictions, setPredictions] = useState(null);
@@ -20,6 +21,7 @@ const Main = (props: any): React.JSXElement => {
   
   const modelRef = useRef();
   const image = useRef();
+  const camera = useRef();
 
   const load = async () => {
     try {
@@ -58,8 +60,6 @@ const Main = (props: any): React.JSXElement => {
     requestCameraPermissions();
   }, []);
   
-  const text = 'TF and model are Ready!';
-
   return (
     <View>
       { loading
@@ -70,6 +70,30 @@ const Main = (props: any): React.JSXElement => {
         ? <Text>We are ready to use camera</Text>
         : <Text>Needs camera permission</Text>
       }
+      <RNCamera
+          // ref={ref => {
+          //   camera.current = ref;
+          // }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+          captureAudio={false}
+        />
     </View>
   )
 }
@@ -79,5 +103,8 @@ export default Main;
 const styles = StyleSheet.create({
   main: {
     display: 'flex'
+  },
+  preview: {
+
   }
 });
